1e77d16866daafd2614eb637e1486338aae225e6
Ryan-Millard <millardryandevon@gmail.com>
2025-02-11 11:16:13 +0200

    Finish daily update for WeatherApp project

--- projects/WeatherApp/WeatherApp.ino
+++ projects/WeatherApp/WeatherApp.ino
@@ -6,6 +6,7 @@
 
 // User-defined
 #include "env.h" // Holds definitions for WIFI_SSID, WIFI_PASSWORD & OPEN_WEATHER_MAP_API_KEY
+#include "lib/Fonts/WeatherIcon.h"
 
 // The below must be defined in env.h
 extern const char* WIFI_SSID; // Name of your WiFi
@@ -28,6 +29,18 @@ String countryCode = "";
 
 // Create Inkplate object
 Inkplate display;
+
+enum WeatherChar : char {
+	CELSIUS = 42,
+	CLEAR = 66,
+	CLOUDS = 78,
+	RAIN = 88,
+	DRIZZLE = 82,
+	THUNDERSTORM = 90,
+	SNOW = 87,
+	MIST = 77,
+	UNKNOWN = 64
+};
 
 void setup() {
 	Serial.begin(115200);
@@ -82,7 +95,6 @@ void setup() {
 		updateWeather();
 	}
 
-	
 	// Enter deep sleep
 	Serial.println("Going to sleep...");
 	esp_sleep_enable_timer_wakeup(SLEEP_TIME * 1000000);  // Set the timer to wake up after 24 hours (in microseconds)
@@ -138,7 +150,7 @@ void updateWeather() {
 		return;
 	}
 
-	Serial.println("Updating weather information...");
+	Serial.println("Fetching weather information...");
 	HTTPClient http;
 	String url = "https://api.openweathermap.org/data/2.5/weather?q=" + 
 		city + "," + countryCode + "&APPID=" + OPEN_WEATHER_MAP_API_KEY + "&units=metric";
@@ -181,33 +193,73 @@ void displayWeather(float temp, float humidity, const char* description, float w
 	display.clearDisplay();
 	display.drawRect(0, 0, display.width(), display.height(), INKPLATE2_BLACK);
 
-	display.setCursor(5, 5);
-	display.setTextColor(INKPLATE2_BLACK); // Using black only in 1-bit mode
-	display.print(temp, 1);
-	display.print("°C");
+	// Heading
+	display.setCursor(12, 5);
+	display.setTextColor(INKPLATE2_BLACK);
+	display.print("Weather Today");
+	// Last time updated
+	struct tm timeinfo;
+	if (getLocalTime(&timeinfo)) {
+		char timeString[9];
+		strftime(timeString, 9, "%H:%M", &timeinfo);
+		display.setCursor(135, 5);
+		display.print("Time: " + String(timeString));
+	}
+	display.drawFastHLine(10, 15, 192, INKPLATE2_BLACK);
 
-	display.setCursor(5, 15);
+	// Weather Image Description
+	if (description != nullptr) {
+		display.setCursor(5, 76);
+		display.setTextSize(3);
+		display.setFont(&WeatherIcon); // Change to weather icon font
+
+		char weatherCharImg = 0;
+		String descriptionUpper = String(description); // Convert to String
+		descriptionUpper.toUpperCase(); // Now call toUpperCase() directly on the String
+
+		if (descriptionUpper == "CLEAR") {
+			weatherCharImg = WeatherChar::CLEAR;
+		} else if (descriptionUpper == "CLOUDS") {
+			weatherCharImg = WeatherChar::CLOUDS;
+		} else if (descriptionUpper == "RAIN") {
+			weatherCharImg = WeatherChar::RAIN;
+		} else if (descriptionUpper == "DRIZZLE") {
+			weatherCharImg = WeatherChar::DRIZZLE;
+		} else if (descriptionUpper == "THUNDERSTORM") {
+			weatherCharImg = WeatherChar::THUNDERSTORM;
+		} else if (descriptionUpper == "SNOW") {
+			weatherCharImg = WeatherChar::SNOW;
+		} else if (descriptionUpper == "MIST") {
+			weatherCharImg = WeatherChar::MIST;
+		} else {
+			weatherCharImg = WeatherChar::UNKNOWN;
+		}
+
+		display.print(weatherCharImg);
+		display.setFont(nullptr); // Return to normal font (use nullptr instead of null)
+	}
+
+	// Temperature
+	display.setTextSize(3);
+	display.setCursor(105, 20);
+	display.print(temp, 1);
+	display.setCursor(160, 56);
+	display.setFont(&WeatherIcon); // Change to weather icon font
+	display.print((char)WeatherChar::CELSIUS);
+	display.setFont(nullptr); // Return to normal font (use nullptr instead of null)
+
+	// Humidity
+	display.setTextSize(1);
+	display.setCursor(5, 89);
 	display.print("Humidity: ");
 	display.print(humidity, 0);
 	display.print("%");
 
-	display.setCursor(5, 25);
-	display.print("Wind: ");
+	// Wind Speed
+	display.setCursor(90, 89);
+	display.print("Wind Speed: ");
 	display.print(windSpeed, 1);
 	display.print(" m/s");
 
-	if (description != nullptr) {
-		display.setCursor(5, 35);
-		display.print(description);
-	}
-
-	struct tm timeinfo;
-	if (getLocalTime(&timeinfo)) {
-		char timeString[9];
-		strftime(timeString, 9, "%H:%M:%S", &timeinfo);
-		display.setCursor(2, 85);
-		display.print("Updated at: " + String(timeString));
-	}
-
-	display.display();
+display.display();
 }
