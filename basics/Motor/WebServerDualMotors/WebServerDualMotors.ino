1a11ee4ccf99a55ee8ed42c40a8be83146e8dd5c
Ryan-Millard <millardryandevon@gmail.com>
2025-03-10 23:36:29 +0200

    Use LittleFS in WebServerDualMotors

--- basics/Motor/WebServerDualMotors/WebServerDualMotors.ino
+++ basics/Motor/WebServerDualMotors/WebServerDualMotors.ino
@@ -2,6 +2,7 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <WebSocketsServer.h>
+#include <LittleFS.h>
 
 // Define pins
 #define EN8V_PIN 16
@@ -27,51 +28,6 @@ WebSocketsServer webSocket = WebSocketsServer(81);
 // Motor objects
 HardwareDevices::Motor motorA(MOTOR_A_PWM_OUT_PIN, MOTOR_A_DIRECTION_PIN, EN8V_PIN, EN5V_PIN, DVR_SLEEP_PIN, true);
 HardwareDevices::Motor motorB(MOTOR_B_PWM_OUT_PIN, MOTOR_B_DIRECTION_PIN, EN8V_PIN, EN5V_PIN, DVR_SLEEP_PIN);
-
-// HTML page
-const char* htmlPage = R"rawliteral(
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Motor Control</title>
-    <style>
-        body { font-family: Arial, sans-serif; margin: 20px; }
-        .slider-container { margin: 20px 0; width: 100%; }
-        input[type="range"] { width: 100%; }
-		#motorA { margin-bottom: 60vh; }
-    </style>
-</head>
-<body>
-    <h1>Motor Control</h1>
-    <div class="slider-container">
-        <label for="motorA">Motor A:</label>
-        <input type="range" id="motorA" min="-255" max="255" value="0"
-               oninput="updateMotor('A', this.value)"
-               onmouseup="this.value=0; updateMotor('A', 0)"
-               ontouchend="this.value=0; updateMotor('A', 0)">
-        <span id="motorAValue">0</span>
-    </div>
-    <div class="slider-container">
-        <label for="motorB">Motor B:</label>
-        <input type="range" id="motorB" min="-255" max="255" value="0"
-               oninput="updateMotor('B', this.value)"
-               onmouseup="this.value=0; updateMotor('B', 0)"
-               ontouchend="this.value=0; updateMotor('B', 0)">
-        <span id="motorBValue">0</span>
-    </div>
-    <script>
-        var ws = new WebSocket('ws://' + window.location.hostname + ':81/');
-
-        function updateMotor(motor, value) {
-            document.getElementById('motor' + motor + 'Value').textContent = value;
-            if (ws.readyState === WebSocket.OPEN) {
-                ws.send(motor + ',' + value);
-            }
-        }
-    </script>
-</body>
-</html>
-)rawliteral";
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length)
 {
@@ -104,6 +60,11 @@ void setup()
 {
 	Serial.begin(115200);
 
+	if (!LittleFS.begin()) {
+		Serial.println("Failed to mount LittleFS");
+		return;
+	}
+
 	// Initialize WiFi AP
 	WiFi.softAP(ssid, password);
 	IPAddress IP = WiFi.softAPIP();
@@ -111,9 +72,17 @@ void setup()
 	Serial.println(IP);
 
 	// Initialize web server
-	server.on("/", []() {
-			server.send(200, "text/html", htmlPage);
-			});
+	server.on("/", HTTP_GET, []() {
+		File file = LittleFS.open("/index.html", "r"); // Open file
+		if(!file)
+		{
+			File file = LittleFS.open("/error.html", "r"); // Open file
+			server.send(500, "text/plain", "Error loading HTML");
+			return;
+		}
+		server.streamFile(file, "text/html"); // Stream the file to the client
+		file.close(); // Close the file
+	});
 	server.begin();
 
 	// Initialize WebSocket
