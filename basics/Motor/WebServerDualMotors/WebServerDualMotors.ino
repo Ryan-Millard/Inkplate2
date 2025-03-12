e302d8f986ce23b343a91b00df6c8751c0e10e8b
Ryan-Millard <millardryandevon@gmail.com>
2025-03-12 17:25:23 +0200

    Final tweaks to WebServerDualMotors

--- basics/Motor/WebServerDualMotors/WebServerDualMotors.ino
+++ basics/Motor/WebServerDualMotors/WebServerDualMotors.ino
@@ -29,25 +29,42 @@ WebSocketsServer webSocket = WebSocketsServer(81);
 HardwareDevices::Motor motorA(MOTOR_A_PWM_OUT_PIN, MOTOR_A_DIRECTION_PIN, EN8V_PIN, EN5V_PIN, DVR_SLEEP_PIN, true);
 HardwareDevices::Motor motorB(MOTOR_B_PWM_OUT_PIN, MOTOR_B_DIRECTION_PIN, EN8V_PIN, EN5V_PIN, DVR_SLEEP_PIN);
 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length)
-{
-	switch(type)
-	{
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+	switch(type) {
 		case WStype_TEXT:
 			{
 				String message = (char*)payload;
 				int commaPos = message.indexOf(',');
 				if (commaPos == -1) return;
 
-				String motor = message.substring(0, commaPos);
-				int speed = message.substring(commaPos + 1).toInt();
+				String part1 = message.substring(0, commaPos);
+				String part2 = message.substring(commaPos + 1);
 
-				Serial.print("Speed: ");
-				Serial.println(speed);
-				if (motor == "A") {
-					motorA.rotate(speed);
-				} else if (motor == "B") {
-					motorB.rotate(speed);
+				// Check if it's a motor command (A or B)
+				if (part1 == "A" || part1 == "B") {
+					int speed = part2.toInt();
+					if (part1 == "A") {
+						motorA.rotate(speed);
+					} else {
+						motorB.rotate(speed);
+					}
+				}
+				// Otherwise, process as joystick data (x,y)
+				else {
+					float x = part1.toFloat();
+					float y = part2.toFloat();
+
+					// Calculate motor speeds for differential drive
+					int leftSpeed = (y + x) * 255;
+					int rightSpeed = (y - x) * 255;
+
+					// Constrain to valid PWM range (-255 to 255)
+					leftSpeed = constrain(leftSpeed, -255, 255);
+					rightSpeed = constrain(rightSpeed, -255, 255);
+
+					// Apply speeds to motors
+					motorA.rotate(leftSpeed);
+					motorB.rotate(rightSpeed);
 				}
 			}
 			break;
@@ -76,7 +93,16 @@ void setup()
 		File file = LittleFS.open("/index.html", "r"); // Open file
 		if(!file)
 		{
-			File file = LittleFS.open("/error.html", "r"); // Open file
+			server.send(500, "text/plain", "Error loading HTML");
+			return;
+		}
+		server.streamFile(file, "text/html"); // Stream the file to the client
+		file.close(); // Close the file
+	});
+	server.on("/joystick", HTTP_GET, []() {
+		File file = LittleFS.open("/joystick.html", "r"); // Open file
+		if(!file)
+		{
 			server.send(500, "text/plain", "Error loading HTML");
 			return;
 		}
